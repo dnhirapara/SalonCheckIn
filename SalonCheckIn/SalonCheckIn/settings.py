c1344cb13e1fe7220149df8bb9dd82108d45fca5
dnhirapara <43730525+dnhirapara@users.noreply.github.com>
2021-01-16 16:42:59 +0530

    settings.py updated

--- SalonCheckIn/SalonCheckIn/settings.py
+++ SalonCheckIn/SalonCheckIn/settings.py
@@ -37,11 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework.authtoken',
 
     # third pary apps
+
     'crispy_forms',
+    'rest_framework',
+    'rest_framework.authtoken',
 
     # My Apps
 
