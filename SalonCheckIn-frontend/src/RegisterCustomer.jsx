082b37b6fdaaf8ba742e5c5d114d4f89355b4020
Bjbrother <nikunjjoshi3701@gmail.com>
2021-01-21 21:10:40 +0530

    frontend added

---
+++ SalonCheckIn-frontend/src/RegisterCustomer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import RegisterSalonCustomer from './RegisterSalonCustomer';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+import Alert from './Alert';
+
+
+
+const RegisterCustomer = () =>{
+
+    const showAlert = (al)=>
+    {
+        alert(al);
+        return (<Alert response={al}/>)
+    }
+    return (
+        <>
+            <RegisterSalonCustomer type="Customer" Show={showAlert}/>
+        </>
+    )
+}
+
+export default RegisterCustomer;
