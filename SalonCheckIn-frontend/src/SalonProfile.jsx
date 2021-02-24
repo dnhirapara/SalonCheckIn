8688fa85321c042c69cc101db56fcbbf81b28490
Bjbrother <nikunjjoshi3701@gmail.com>
2021-02-24 09:41:10 +0530

    added timeline icon

--- SalonCheckIn-frontend/src/SalonProfile.jsx
+++ SalonCheckIn-frontend/src/SalonProfile.jsx
@@ -3,11 +3,13 @@ import axios from 'axios';
 import Alert from './Alert';
 import { Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import Navbar2 from './Navbar2';
+import TimeLine from './TimeLine';
 
 const SalonProfile = () => {
     return (
         <>
         <Navbar2/>
+        <TimeLine/>
 
 
         </>
