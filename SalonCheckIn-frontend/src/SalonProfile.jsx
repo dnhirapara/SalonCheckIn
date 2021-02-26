d1c4d96e8df01abb5b2d6bbe6cb613741929970d
dnhirapara <43730525+dnhirapara@users.noreply.github.com>
2021-02-26 22:13:39 +0530

    bug fixes

--- SalonCheckIn-frontend/src/SalonProfile.jsx
+++ SalonCheckIn-frontend/src/SalonProfile.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import axios from 'axios';
-import Alert from './Alert';
-import { Redirect, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
-import Navbar2 from './Navbar2';
-import TimeLine from './TimeLine';
+import React from "react";
+import axios from "axios";
+import Alert from "./Alert";
+import {
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
+import Navbar2 from "./Navbar2";
+import TimeLine from "./TimeLine";
 
 const SalonProfile = () => {
-    return (
-        <>
-        <Navbar2/>
-        <TimeLine/>
-        
-        
-
-        </>
-        
-    );
+  return (
+    <>
+      <Navbar2 />
+    </>
+  );
 };
 
 export default SalonProfile;
--- SalonCheckIn-frontend/src/TimeLine.jsx
+++ SalonCheckIn-frontend/src/TimeLine.jsx
@@ -1,109 +1,109 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Timeline from '@material-ui/lab/Timeline';
-import TimelineItem from '@material-ui/lab/TimelineItem';
-import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
-import TimelineConnector from '@material-ui/lab/TimelineConnector';
-import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineOppositeContent from '@material-ui/lab/TimelineOppositeContent';
-import TimelineDot from '@material-ui/lab/TimelineDot';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Timeline from "@material-ui/lab/Timeline";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent";
+import TimelineDot from "@material-ui/lab/TimelineDot";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
-	paper: {
-		padding: '6px 16px'
-	},
-	secondaryTail: {
-		backgroundColor: theme.palette.secondary.main
-	}
+  paper: {
+    padding: "6px 16px",
+  },
+  secondaryTail: {
+    backgroundColor: theme.palette.secondary.main,
+  },
 }));
 
 export default function TimeLine() {
-	const classes = useStyles();
+  const classes = useStyles();
 
-	return (
-        <>
-        <div className="container-fluid">
+  return (
+    <>
+      <div className="container-fluid">
         <h2 className="text-center">Your Appointments of Today</h2>
-		<Timeline align='alternate'>
-			<TimelineItem>
-				<TimelineOppositeContent>
-					<Typography variant='body2' color='textSecondary'>
-						9:30 am
-					</Typography>
-				</TimelineOppositeContent>
-				<TimelineSeparator>
-					<TimelineDot />
-					<TimelineConnector />
-				</TimelineSeparator>
-				<TimelineContent>
-					<Paper elevation={6} className={classes.paper}>
-						<Typography variant='h6' component='h1'>
-							Appointment-1
-						</Typography>
-						<Typography>Frodo</Typography>
-					</Paper>
-				</TimelineContent>
-			</TimelineItem>
-			<TimelineItem>
-				<TimelineOppositeContent>
-					<Typography variant='body2' color='textSecondary'>
-						10:00
-					</Typography>
-				</TimelineOppositeContent>
-				<TimelineSeparator>
-					<TimelineDot color='primary' />
-					<TimelineConnector />
-				</TimelineSeparator>
-				<TimelineContent>
-					<Paper elevation={3} className={classes.paper}>
-						<Typography variant='h6' component='h1'>
-                        Appointment-2
-						</Typography>
-						<Typography>Sam</Typography>
-					</Paper>
-				</TimelineContent>
-			</TimelineItem>
-			<TimelineItem>
+        <Timeline align="alternate">
+          <TimelineItem>
             <TimelineOppositeContent>
-					<Typography variant='body2' color='textSecondary'>
-						10:30
-					</Typography>
-				</TimelineOppositeContent>
-				<TimelineSeparator>
-					<TimelineDot color='primary' variant='outlined' />
-					<TimelineConnector className={classes.secondaryTail} />
-				</TimelineSeparator>
-				<TimelineContent>
-					<Paper elevation={3} className={classes.paper}>
-						<Typography variant='h6' component='h1'>
-							Appointment-3
-						</Typography>
-						<Typography>Gandalf</Typography>
-					</Paper>
-				</TimelineContent>
-			</TimelineItem>
-			<TimelineItem>
+              <Typography variant="body2" color="textSecondary">
+                9:30 am
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper elevation={6} className={classes.paper}>
+                <Typography variant="h6" component="h1">
+                  Appointment-1
+                </Typography>
+                <Typography>Frodo</Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
             <TimelineOppositeContent>
-					<Typography variant='body2' color='textSecondary'>
-						11:00
-					</Typography>
-			</TimelineOppositeContent>
-				<TimelineSeparator>
-					<TimelineDot color='secondary' />
-				</TimelineSeparator>
-				<TimelineContent>
-					<Paper elevation={2} className={classes.paper}>
-						<Typography variant='h6' component='h1'>
-							Appointment-4
-						</Typography>
-						<Typography>zen</Typography>
-					</Paper>
-				</TimelineContent>
-			</TimelineItem>
-		</Timeline>
-        </div>
-        </>
-	);
+              <Typography variant="body2" color="textSecondary">
+                10:00
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="primary" />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper elevation={3} className={classes.paper}>
+                <Typography variant="h6" component="h1">
+                  Appointment-2
+                </Typography>
+                <Typography>Sam</Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" color="textSecondary">
+                10:30
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="primary" variant="outlined" />
+              <TimelineConnector className={classes.secondaryTail} />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper elevation={3} className={classes.paper}>
+                <Typography variant="h6" component="h1">
+                  Appointment-3
+                </Typography>
+                <Typography>Gandalf</Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent>
+              <Typography variant="body2" color="textSecondary">
+                11:00
+              </Typography>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot color="secondary" />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Paper elevation={2} className={classes.paper}>
+                <Typography variant="h6" component="h1">
+                  Appointment-4
+                </Typography>
+                <Typography>zen</Typography>
+              </Paper>
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
+      </div>
+    </>
+  );
 }
